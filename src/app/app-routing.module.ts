b7b0095ab9126686ac4bfaf1fc4a5206572f4a21
vignesh0208 <v19ne5h1994@gmail.com>
2019-07-23 18:24:05 +0530

    removed error in cards and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'conversation', loadChildren: './conversation/conversation.module#ConversationPageModule' },
-  { path: 'modal', loadChildren: './modal/modal.module#ModalPageModule' },
-  { path: 'chat', loadChildren: './conversation/chat/chat.module#ChatPageModule' },
-  { path: 'chatpage', loadChildren: './conversation/chatpage/chatpage.module#ChatpagePageModule' }
+  { path: 'modal', loadChildren: './modal/modal.module#ModalPageModule' }
 ];
 
 @NgModule({
--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -29,4 +29,10 @@ export class ServicesService {
     var color = Math.floor(0x1000000 * Math.random()).toString(16);
     return '#' + ('000000' + color).slice(-6);
   }
+  
+  getIdnumValue(id:number) {
+    return this.dataValue.filter(val => {
+        return val.id === id;
+    })[0]
+  }
 }
